c19520a130de46f8d564acf67ff1b42ab8e2e5c3
Sergiu Verciuc <v_sergiu2009@yahoo.com>
2020-05-24 19:08:53 +0300

    Additional functionality for mongo

--- Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Persistence.Mongo/IMongoStorage.cs
+++ Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Persistence.Mongo/IMongoStorage.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Cegeka.Guild.Pokeverse.Common;
+using CSharpFunctionalExtensions;
 
 namespace Cegeka.Guild.Pokeverse.Persistence.Mongo
 {
     public interface IMongoStorage
     {
-        Task Create<T>(T entity) where T : DocumentModel, new();
-        
-        Task Update<T>(string id, Action<T> update) where T : DocumentModel, new();
+        Task<IEnumerable<T>> GetAll<T>()
+            where T : DocumentModel, new();
 
-        Task Update<T>(T instance) where T : DocumentModel, new();
+        Task<IEnumerable<T>> Find<T>(Expression<Func<T, bool>> predicate)
+            where T : DocumentModel, new();
+
+        Task<Maybe<T>> FindOne<T>(Expression<Func<T, bool>> predicate)
+            where T : DocumentModel,  new();
+
+        Task Create<T>(T entity) 
+            where T : DocumentModel, new();
         
-        Task Delete<T>(string id) where T : DocumentModel, new();
+        Task Update<T>(string id, Action<T> update) 
+            where T : DocumentModel, new();
+
+        Task Update<T>(T instance) 
+            where T : DocumentModel, new();
+        
+        Task Delete<T>(string id)
+            where T : DocumentModel, new();
     }
 }
--- Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Persistence.Mongo/MongoStorage.cs
+++ Cegeka.Guild.Pokeverse/Cegeka.Guild.Pokeverse.Persistence.Mongo/MongoStorage.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Cegeka.Guild.Pokeverse.Common;
+using CSharpFunctionalExtensions;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Polly;
 
 namespace Cegeka.Guild.Pokeverse.Persistence.Mongo
 {
@@ -15,6 +20,38 @@ namespace Cegeka.Guild.Pokeverse.Persistence.Mongo
         {
             this.settings = settings;
             this.client = client;
+        }
+
+        public async Task<IEnumerable<T>> GetAll<T>()
+            where T : DocumentModel, new()
+        {
+            return await GetCollection<T>().AsQueryable().ToListAsync();
+        }
+
+        public Task<IEnumerable<T>> Find<T>(Expression<Func<T, bool>> predicate)
+            where T : DocumentModel, new()
+        {
+            var collectionQuery = GetCollection<T>().AsQueryable();
+            return Policy<IEnumerable<T>>.Handle<Exception>()
+                .FallbackAsync(collectionQuery.Where(predicate.Compile()))
+                .ExecuteAsync(async () => await collectionQuery.Where(predicate).ToListAsync());
+        }
+
+        public async Task<Maybe<T>> FindOne<T>(Expression<Func<T, bool>> predicate) 
+            where T : DocumentModel, new()
+        {
+            var aggregateOrNothing = Maybe<T>.None;
+            var collectionQuery = GetCollection<T>().AsQueryable();
+            try
+            {
+                aggregateOrNothing = await collectionQuery.FirstOrDefaultAsync(predicate);
+            }
+            catch
+            {
+                aggregateOrNothing = collectionQuery.Where(predicate.Compile()).FirstOrDefault();
+            }
+
+            return aggregateOrNothing;
         }
 
         public Task Create<T>(T entity) where T : DocumentModel, new() => GetCollection<T>().InsertOneAsync(entity);
